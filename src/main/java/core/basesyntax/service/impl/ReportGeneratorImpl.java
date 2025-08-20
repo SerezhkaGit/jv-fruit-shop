b71e095f7c7862eecffd4cd09c8866c590234b15
Serhiy <sergiy.kusiv@gmail.com>
2025-08-20 13:56:26 +0300

    Refactor a ReportGeneratorImpl

--- src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
@@ -5,12 +5,14 @@ import core.basesyntax.service.ReportGenerator;
 import java.util.Map;
 
 public class ReportGeneratorImpl implements ReportGenerator {
+    private static final char COMMA = ',';
+    private static final String HEADER = "fruit,quantity" + System.lineSeparator();
     @Override
     public String getReport() {
         StringBuilder report = new StringBuilder("fruit,quantity\n");
         for (Map.Entry<String, Integer> entry : Storage.fruits.entrySet()) {
-            report.append(entry.getKey()).append(",")
-                    .append(entry.getValue()).append("\n");
+            report.append(entry.getKey()).append("COMMA")
+                    .append(entry.getValue()).append(System.lineSeparator());
         }
         return report.toString();
     }
