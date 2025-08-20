d9e6f0de77e381895695339d7984086f82a8b81c
Serhiy <sergiy.kusiv@gmail.com>
2025-08-20 13:57:33 +0300

    solved checkstyle issues in ReportGeneratorImpl

--- src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
@@ -7,6 +7,7 @@ import java.util.Map;
 public class ReportGeneratorImpl implements ReportGenerator {
     private static final char COMMA = ',';
     private static final String HEADER = "fruit,quantity" + System.lineSeparator();
+
     @Override
     public String getReport() {
         StringBuilder report = new StringBuilder("fruit,quantity\n");
