a43693a7ab336f92836c663562d7d3cb42db815a
Serhiy <sergiy.kusiv@gmail.com>
2025-08-21 14:16:36 +0300

    created constant for | DEFAULT_VALUE | in PuchaseOperation

--- src/main/java/core/basesyntax/strategy/PurchaseOperation.java
+++ src/main/java/core/basesyntax/strategy/PurchaseOperation.java
@@ -4,10 +4,12 @@ import core.basesyntax.db.Storage;
 import core.basesyntax.model.FruitTransaction;
 
 public class PurchaseOperation implements OperationHandler {
+    private static final int DEFAULT_VALUE = 0;
+
     @Override
     public void apply(FruitTransaction transaction) {
         String fruit = transaction.getFruit();
-        int currentQuantity = Storage.fruits.getOrDefault(fruit, 0);
+        int currentQuantity = Storage.fruits.getOrDefault(fruit, DEFAULT_VALUE);
         int toBuy = transaction.getQuantity();
 
         if (currentQuantity < toBuy) {
